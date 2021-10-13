74afe6402485412059dba88f03dbdfedebf6ee3b
Marina Dariy <dariy.mvs@gmail.com>
2021-10-13 15:09:55 +0300

    part2

--- #src/js/index.js
+++ #src/js/index.js
@@ -2,10 +2,10 @@ $( document ).ready( () => {
   $( '.hamburger' ).on( 'click', function() {
     if ( !$( this ).hasClass( 'is-active' ) ) {
       $( this ).addClass( 'is-active' );
-      $( '.header__nav_list' ).fadeIn( 500 );
+      $( '.header__menu' ).fadeIn( 500 );
     } else {
       $( this ).removeClass( 'is-active' );
-      $( '.header__nav_list' ).fadeOut( 500 );
+      $( '.header__menu' ).fadeOut( 500 );
     }
   } );
 
@@ -52,20 +52,42 @@ $( document ).ready( () => {
   function getNextSlide(event) {
     console.log(this.btn);
     let newsList = document.querySelector('.submenu__news_list');
-    let newsArr = [...document.querySelectorAll('.submenu__news_item')];
+    let newsItems = [...document.querySelectorAll('.submenu__news_item')].length - 1;
     let newsWidth = document.querySelector('.submenu__news_item').clientWidth;
     console.log(newsWidth);
     let offset = parseInt(newsList.style.left, 10) || 0;
     console.log(offset);
+
     if (this.btn === 'next') {
-      offset <= -newsWidth * newsArr.length ? newsList.style.left = 0 :  newsList.style.left = offset - newsWidth + 'px';
+      offset <= -newsWidth * newsItems ? newsList.style.left = 0 + 'px' :  newsList.style.left = offset - newsWidth + 'px';
     } else if (this.btn === 'prev') {
-      offset >= newsWidth * newsArr.length ? newsList.style.left = 0 :  newsList.style.left = offset + newsWidth + 'px';
+      offset === 0 ? newsList.style.left = -newsWidth * newsItems + 'px' :  newsList.style.left = offset + newsWidth + 'px';
     }
   }
   
   document.querySelector('.pagination_news_next').addEventListener('click', {handleEvent: getNextSlide, btn: 'next'});
   document.querySelector('.pagination_news_prev').addEventListener('click', {handleEvent: getNextSlide, btn: 'prev'});
+
+  // открытие сабменю
+  if ( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test( navigator.userAgent ) ) {
+    // $( '.catalog-home__chapter_descr' ).each( function() {
+    //   $( this ).addClass( 'chapterMobile' );
+    // } );
+
+    // переходить по ссылке только после открытия меню
+    $( '.header__menu_item' ).on( 'click', ( evt ) => {
+      const target = evt.target.closest( '.header__menu_item' );
+      const subMenu = [ ...target.children ].find( ( el ) => el.classList.contains( 'submenu' ) );
+      if ( subMenu ) {
+        if ( !target.classList.contains( 'active_menu' ) ) {
+          evt.preventDefault();
+          target.classList.add( 'active_menu' );
+        } else {
+          target.classList.remove( 'active_menu' );
+        }
+      }
+    } );
+  }
 
 
 } );
