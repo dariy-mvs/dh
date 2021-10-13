defe87ad6943b5ee508b094071759ba57992edb0
Marina Dariy <dariy.mvs@gmail.com>
2021-10-13 08:37:59 +0300

    1

--- #src/js/index.js
+++ #src/js/index.js
@@ -33,19 +33,39 @@ $( document ).ready( () => {
     $( '.pagination-container' ).pagination( {
       items: numItems,
       itemsOnPage: perPage,
-      prevText: '&laquo;',
-      nextText: '&raquo;',
+      //prevText: '&laquo;',
+      //nextText: '&raquo;',
+      prevText: ' ',
+      nextText: ' ',
       onPageClick( pageNumber ) {
         const showFrom = perPage * ( pageNumber - 1 );
         const showTo = showFrom + perPage;
         items.hide().slice( showFrom, showTo ).show();
-        $( 'html, body' ).animate( { scrollTop: 0 }, 100 );
+        // $( 'html, body' ).animate( { scrollTop: 0 }, 100 );
       }
     } );
   }
 
   pagination( '.submenu__catalogPdf_list', '.submenu__catalogPdf_item', 3 );
-  pagination( '.submenu__news_list', '.submenu__news_item', 1 );
-  // pagination( '.catalog__list', '.catalog__item', 5 );
+
+  // переключение активной новости
+  function getNextSlide(event) {
+    console.log(this.btn);
+    let newsList = document.querySelector('.submenu__news_list');
+    let newsArr = [...document.querySelectorAll('.submenu__news_item')];
+    let newsWidth = document.querySelector('.submenu__news_item').clientWidth;
+    console.log(newsWidth);
+    let offset = parseInt(newsList.style.left, 10) || 0;
+    console.log(offset);
+    if (this.btn === 'next') {
+      offset <= -newsWidth * newsArr.length ? newsList.style.left = 0 :  newsList.style.left = offset - newsWidth + 'px';
+    } else if (this.btn === 'prev') {
+      offset >= newsWidth * newsArr.length ? newsList.style.left = 0 :  newsList.style.left = offset + newsWidth + 'px';
+    }
+  }
+  
+  document.querySelector('.pagination_news_next').addEventListener('click', {handleEvent: getNextSlide, btn: 'next'});
+  document.querySelector('.pagination_news_prev').addEventListener('click', {handleEvent: getNextSlide, btn: 'prev'});
+
 
 } );
